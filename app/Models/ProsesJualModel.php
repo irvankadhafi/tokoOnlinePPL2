6006afaa26bd618e4a19e0b5ae911e874ea58175
Irvan Kadhafi <irvan.kadhafi.tif18@polban.ac.id>
2021-01-05 13:22:03 +0700

    update stock when proccess order

--- app/Models/ProsesJualModel.php
+++ app/Models/ProsesJualModel.php
@@ -6,15 +6,20 @@ use CodeIgniter\Model;
 class ProsesJualModel extends Model{
 
     protected $table = "proses_jual";
-
     public function insertProsesJual($data)
     {
         $id = $data['idtrx'];
         $items = $data['items'];
+
         foreach($items as $key => $item) {
             $productId = $item['id'];
             $productPrice = $item['price'];
             $productQty = $item['quantity'];
+
+            $productStock = $this->db->table('products')->where('id', $productId)->get()->getRowArray();
+            $nowStock = $productStock['stock'] - $productQty;
+
+            $this->db->query("UPDATE products SET stock = '$nowStock' where id = '$productId'");
             $this->db->query("INSERT INTO proses_jual(id_penjualan, id_produk, price, qty) VALUES ('$id','$productId','$productPrice','$productQty')");
         }
     }
